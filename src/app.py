8384b3121428d6e9fd5c4cbcd832a0a32fb8eaba
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2022-09-22 21:42:25 -0400

    Added Listing Page / Preliminary Settings

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,3 +7,11 @@ app = Flask(__name__)
 
 def home():
     return render_template('homepage.html')
+
+@app.route("/listing")
+def listing():
+    return render_template('listing.html')
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
