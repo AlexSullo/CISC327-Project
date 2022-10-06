4501aa587b23f52a66754f4c79d4562c357afea1
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2022-10-06 19:04:39 -0400

    Updated HTML Structure, Added all Requirements
    HTML files now use a template.
    
    R3-1: User can only update username, email, address, and postal code :white_check_mark:
    
    R3-2: Postal code should be non-empty, alphanum. only, and no special characters :white_check_mark:
    
    R3-3: Postal code has to be valid Canadian postal code :white_check_mark:
    
    R3-4: Username must be nonempty, alphanum. only, and no special characters :white_check_mark:
    
    NOTE:
    Awaiting a response from Steven about whether or not we have to code our automated tests yet, so I'm going to push this now and wait.

--- qbnb_test/conftest.py
+++ qbnb_test/conftest.py
@@ -1,1 +1,12 @@
-# CODE TO RUN BEFORE/AFTER ALL TESTING
+from qbnb import *
+from qbnb.models import *
+from qbnb_test import *
+
+
+def open_server():
+    test_db = SQLAlchemy(test_app)
+    test_db.session.create_all()
+    test_app.run()
+
+
+open_server()
