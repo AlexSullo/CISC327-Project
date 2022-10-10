63719940d7445d3e28c6d0ffb5ad6ea68ba62d65
AlexSullo <20awas@queensu.ca>
2022-10-10 13:31:09 -0400

    Update models.py
    Fixes PEP8 issues

--- qbnb/models.py
+++ qbnb/models.py
@@ -19,47 +19,47 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, 
-                   primary_key=True,
-                   unique=True)
+        primary_key=True,
+        unique=True)
 
     username = db.Column(db.String(80), 
-                         unique=True, 
-                         nullable=False)
+        unique=True, 
+        nullable=False)
 
     email = db.Column(db.String(120), 
-                      unique=True, 
-                      nullable=False)
+        unique=True, 
+        nullable=False)
 
     password = db.Column(db.String(120), 
-                         unique=True, 
-                         nullable=False)
+        unique=True, 
+        nullable=False)
 
     rating = db.Column(db.String(120), 
-                       unique=True, 
-                       nullable=False)
+        unique=True, 
+        nullable=False)
 
     propertyReview = db.Column(db.String(120), 
-                               unique=True, 
-                               nullable=False)
+        unique=True, 
+        nullable=False)
     
     shippingAddress = db.Column(db.String(120),
-                                unique=False,
-                                nullable=True)
+        unique=False,
+        nullable=True)
 
     postalCode = db.Column(db.String(120),
-                            unique=False,
-                            nullable=True)
+        unique=False,
+        nullable=True)
 
     balance = db.Column(db.Integer(),
-                        unique=False,
-                        nullable=False)
+        unique=False,
+        nullable=False)
 
     userReview = db.Column(db.String(120), 
                            unique=True, 
                            nullable=False)
         
     def registration(self, username, password, email):
-        reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+        reg="^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
         pat = re.compile(reg)
         mat = re.search(pat, password)
         if username == "":
@@ -67,12 +67,13 @@ class User(db.Model):
         if password == "":
             print("Password can not be empty.")
         if not mat:
-            print("password is invalid, must contain one lower, one upper, one special char and at least 6 characters long")
+            print("password is invalid, must contain one lower, \
+            one upper, one special char and at least 6 characters long")
         if len(username) < 2  or len(username) > 20:
             print("Username must be between 2 and 20 characters long")
         i = 0
         for c in username:
-            if (i == 0 or i == len(username)-1):  # If its the first or last character
+            if (i == 0 or i == len(username) - 1): 
                 if (not c.isalnum()):  # If its not alphanumeric
                     print("Username: 'contains spaces on the ends or non-alphanumeric'")
                     return False
@@ -209,6 +210,7 @@ class BankTransfer(db.Model):
     transactionAmount = db.Column(db.Float, 
                                   unique=False,
                                   nullable=False)
+
 
 """
 * Sprint two: user registration
