5933ab198928013a95314ff3019daef979428c39
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2022-10-27 10:51:41 -0400

    Tested code
    Still needs some fixes that will be looked into soon.
    EXPLANATION OF CHANGES
    - listingId should not be named listingId as it confuses the DB
    - the dates have been changed to strings for now because the DateTime stuff isn't working properly, but if you could figure that out and change it back before the merge that would be awesome
    - Added commit command so new listings actually save to the database.

--- qbnb/models.py
+++ qbnb/models.py
@@ -241,7 +241,7 @@ class Listing(db.Model):
     """
 
     __tablename__ = 'listings'
-    listingId = db.Column(db.Integer,  # Unique number identifies the listing
+    id = db.Column(db.Integer,  # Unique number identifies the listing
                           primary_key=True,
                           unique=True,
                           nullable=False)
@@ -258,7 +258,7 @@ class Listing(db.Model):
                       unique=False,
                       nullable=False)
 
-    lastModifiedDate = db.Column(db.DateTime,  # The date changes were made
+    lastModifiedDate = db.Column(db.String(60),  # The date changes were made
                                  unique=False,
                                  nullable=False)
 
@@ -302,7 +302,7 @@ class Listing(db.Model):
                         unique=False,
                         nullable=True)
 
-    dateAvailable = db.Column(db.DateTime,  # Range when the property is avail.
+    dateAvailable = db.Column(db.String(60),  # When the property is avail.
                               unique=False,
                               nullable=False)
 
@@ -313,7 +313,6 @@ class Listing(db.Model):
                                 nullable=False)
 
     def __init__(self, listingData):
-        self.listingId = hash(listingData['title'])
         self.title = listingData['title']
         self.description = listingData['description']
         self.price = float(listingData['price'])
@@ -329,6 +328,8 @@ class Listing(db.Model):
         self.rating = '0.0'
         self.reviews = ''
         self.dateAvailable = str(listingData['dateAvailable'])
+        db.session.add(self)
+        db.session.commit()
 
     def checkListing(self):
         """This function checks if the title, description, price, and
