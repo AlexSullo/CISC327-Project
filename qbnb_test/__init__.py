52aa22b9aaee0405d38cc5b85d346d3c26118e08
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2022-11-01 22:02:55 -0400

    fixing again

--- qbnb_test/__init__.py
+++ qbnb_test/__init__.py
@@ -1,7 +1,3 @@
 '''
 Required for the folder to be considered a module
 '''
-from flask import Flask
-test_app = Flask(__name__)
-test_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../testdb.sqlite'
-test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
