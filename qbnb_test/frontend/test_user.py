c2bd9b75d75afdea0a2be5d9e10082d54d39c25e
AlexSullo <alexsullo67@gmail.com>
2022-11-13 18:20:29 -0500

    Front end registration
    These are the tests for the front end registration

--- qbnb_test/frontend/test_user.py
+++ qbnb_test/frontend/test_user.py
@@ -31,7 +31,321 @@ class FrontEndHomePageTest(BaseCase):
 
     #     self.open(base_url)
     #     self.assert_element(".standard-button")
+    """
+    This is the testing for the frontend registration page by Alessandro Sullo (#20236304).
 
+    The three tests I used are:
+        1.input partitioning
+        2. Random Shotgun Testing 
+        3. Boundary testing 
+
+    For testing of the email I used input partitioning,
+
+    Partitions used:
+        1. Valid regex 
+        2. Invalid regex
+        3. Email empty
+    """
+    def test_Email_True(self, *_):
+        """
+        Test for when the email follows all the specified ruled
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-1 test True
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        self.type("#password", "Password$1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(5)
+        self.save_screenshot('RegisterEmail_pass', 'test_screenshots')
+        self.assert_text("QBNB is not a real service")
+
+    def test_Email_False(self, *_):
+        """
+        Test for whne the email contains characters in places 
+        they should not be for example in this case there is 
+        a space and number where the address should be.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-3 test false, does not allow registration
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testUser@gma 1il.com")
+        self.type("#password", "Password!")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(5)
+        self.save_screenshot('RegisterEmail_regexfail', 'test_screenshots')
+        url = self.get_current_url()
+        self.assert_true(url == (base_url + "/register"))
+        print("EMAIL IS NOT VALID.")
+
+    def test_EmailEmpty(self, *_):
+        """
+        Test for when Email is left blank.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-1 test False
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "")
+        self.type("#password", "Password")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.save_screenshot('registerEmail_Empty', 'test_screenshots')
+        url = self.get_current_url()
+        self.assert_true(url == (base_url + "/register"))
+        print("EMAIL CAN NOT BE EMPTY.")
+
+    """
+    Password: uses Systematic Shotgun Testing & Boundary Testing (2&3)
+
+    Partitions: 
+        - Randomly generated (fails regex: at least one lowercase) SST
+        - Randomly generated (fails regex: At least one upper case) SST
+        - Randomly generated (fails regex: At least one digit) SST
+        - Randomly generated (fails specification: too short) SST/BT
+        - Manually passed (passes Regex) IP
+    """
+    def test_Password_True(self, *_):
+        """
+        Test for when password meets all requirments.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-1 test True
+        self.type("#username", "AlexSull")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser2@gmail.com")
+        self.type("#password", "Password$1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(5)
+        self.save_screenshot('registerPassword_regexpass', 'test_screenshots')
+        self.assert_text("QBNB is not a real service")
+
+    def test_Password_FalseDIG(self, *_):
+        """
+        Test for when the password meets all requirments except 
+        containing one Digit.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-4 test False
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        choicesStr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        usernameLen = random.randint(8, 16)
+        rngPassword = ''  # The randomly-generated password
+        for x in range(usernameLen):
+            if x == usernameLen // 2:
+                rngPassword += "lol"
+            else:
+                val = random.randint(1, len(choicesStr))
+                key = choicesStr[val - 1]
+                rngPassword += key
+        self.type("#password", rngPassword)
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(10)
+        self.save_screenshot('registerPassword_regexfail', 'test_screenshots')
+        url = self.get_current_url()
+        self.assert_true(url == (base_url + "/register"))
+        print("PASSWORD MUST CONTAIN A DIGIT.")
+
+    def test_Password_FalseUpper(self, *_):
+        """
+        Test for when the password meets all requirments except 
+        containing one Uppercase.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-4 test False
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        choicesStr = 'abcdefghijklmnopqrstuvwxyz'
+        usernameLen = random.randint(8, 16)
+        rngPassword = ''  # The randomly-generated password
+        for x in range(usernameLen):
+            if x == usernameLen // 2:
+                rngPassword += "lol"
+            else:
+                val = random.randint(1, len(choicesStr))
+                key = choicesStr[val - 1]
+                rngPassword += key
+        self.type("#password", rngPassword)
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.save_screenshot('registerPassword_regexfail', 'test_screenshots')
+        url = self.get_current_url()
+        self.assert_true(url == (base_url + "/register"))
+        print("PASSWORD MUST CONTAIN A UPPER CASE.")
+
+    def test_Password_FalseLenShort(self, *_):
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        # R1-4 test False
+        self.type("#username", "AlexSu")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        choicesStr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        passwordLen = 2
+        rngPassword = ''  # The randomly-generated password
+        for x in range(passwordLen):
+            val = random.randint(1, len(choicesStr))
+            key = choicesStr[val - 1]
+            rngPassword += key
+        self.type("#password", rngPassword)
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.save_screenshot('registerPassword_lengthShortFail', 'test_screenshots')
+
+    """
+    USERNAME: uses Systematic Shotgun Testing & Boundary Testing (2&3)
+
+    Partitions: 
+        - Randomly generated (contains a space as a prefix) SST
+        - Randomly generated (fails specification: too short) SST/BT
+        - Randomly generated (fails specification: too long) SST/BT
+        - selected input (passes Regex) IP
+    """
+
+    def test_Username_True(self, *_):
+        """
+        test for when the username follows the mandatory 
+        regex.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        self.type("#username", "AlexSullo")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser3@gmail.com")
+        self.type("#password", "Password$1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(5)
+        self.save_screenshot('registerUsername_regexpass', 'test_screenshots')
+        self.assert_text("QBNB is not a real service")
+
+    def test_Username_EmptyFalse(self, *_):
+        """
+        Test for when The Username is empty returns fals 
+        does not register you.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        self.type("#username", "")
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        self.type("#password", "Password!1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(3)
+        self.save_screenshot('registerUsername_regexEmptyfail', 'test_screenshots')
+
+    def test_Username_LengthOverFalse(self, *_):
+        """
+        This is the test for when the Username does not 
+        meet the constraints for length.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        choicesStr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        usernameLen = random.randint(21, 32)
+        rngUsername = ''  # The randomly-generated password
+        for x in range(usernameLen):
+            if x == usernameLen // 2:
+                rngUsername += '2'
+            val = random.randint(1, len(choicesStr))
+            key = choicesStr[val - 1]
+            rngUsername += key
+        self.type("#username", rngUsername)
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        self.type("#password", "Password!1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(3)
+        self.save_screenshot('registerUsername_lengthOverfail', 'test_screenshots')
+
+    def test_Username_LengthShortFalse(self, *_):
+        """
+        This is the test for when the Username does not 
+        meet the constraints for length.
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        choicesStr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        usernameLen = 2
+        rngUsername = ''  # The randomly-generated password
+        for x in range(usernameLen):
+            val = random.randint(1, len(choicesStr))
+            key = choicesStr[val - 1]
+            rngUsername += key
+        self.type("#username", rngUsername)
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        self.type("#password", "Password!1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(3)
+        self.save_screenshot('registerUsername_lengthShortfail', 'test_screenshots')
+
+    def test_Username_SpacePreFalse(self, *_):
+        """
+        This is the test for when the Username contains a space
+        as a prefix and fails 
+        """
+        base_url = 'http://127.0.0.1:{}'.format(5000)
+        self.open(base_url + "/register") 
+        choicesStr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        choicesStr += '0123456789!#@-'
+        usernameLen = random.randint(8, 16)
+        rngUsername = ' '  # The randomly-generated password
+        for x in range(usernameLen):
+            val = random.randint(1, len(choicesStr))
+            key = choicesStr[val - 1]
+            rngUsername += key
+        self.type("#username", rngUsername)
+        self.type("#firstName", "Alex")
+        self.type("#surname", "Sullo")
+        self.type("#email", "testuser@gmail.com")
+        self.type("#password", "Password!1")
+        self.type("#billingAddress", "100 real st")
+        self.type("#postalCode", "N7L1W9")
+        self.click("#register-button")
+        self.wait(3)
+        self.save_screenshot('registerUsername_SpacePrefail', 'test_screenshots')
+    
     '''
     User Updating Tests (Sebastian Deluca #20250909)
     email, username, billingaddress, and postalcode.
@@ -466,4 +780,5 @@ class FrontEndHomePageTest(BaseCase):
     BILLING ADDRESS: Will not be tested as there are no given
     restrictions on what can be inputted as a billing address.
     '''
+    
     
