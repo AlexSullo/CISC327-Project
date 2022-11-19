e204a3d7aac9b7563cd26d4e7d0a8099d42d6594
Lucas Papadatos <lucaspapadatos@gmail.com>
2022-11-18 20:58:12 -0500

    sql injection for registration

---
+++ qbnb_test/test_sql_injection.py
@@ -0,0 +1,42 @@
+from qbnb_test.conftest import pytest_sessionstart, pytest_sessionfinish
+from qbnb.models import User
+
+def test_sql_injection():
+    pytest_sessionstart()
+    with open('qbnb_test/sql-injection-payload-list.txt','r') as file:
+        for payload in file:
+            print("Payload:", payload)
+            # SQL INJECTION TESTING: REGISTER (registration in models.py)
+            # try to register with payload as the username
+            userData = {
+                "username": payload,
+                "email": "randomemail@email.com",
+                "password": "testedPassword1!"
+            }
+            try:
+                User.registration(userData)
+            except Exception as error:
+                print("Exception error for username payload:", str(error))
+            # try to register with payload as the email
+            userData = {
+                "username": "testuser",
+                "email": payload,
+                "password": "testedPassword1!"
+            }
+            try:
+                User.registration(userData)
+            except Exception as error:
+                print("Exception error for email payload:", str(error))
+            # try to register with payload as the password
+            userData = {
+                "username": "testuser",
+                "email": "randomemail@email.com",
+                "password": payload
+            }
+            try:
+                User.registration(userData)
+            except Exception as error:
+                print("Exception error for password payload:", str(error))
+            
+            # SQL INJECTION TESTING: LISTING
+    pytest_sessionfinish()
