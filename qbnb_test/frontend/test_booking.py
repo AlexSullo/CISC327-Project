12c00d79fceee87aebd7bb8181257da6e37d0163
artie7lomonaco <artie.lomonaco@gmail.com>
2022-12-05 22:49:05 -0500

    tmp[

--- qbnb_test/frontend/test_booking.py
+++ qbnb_test/frontend/test_booking.py
@@ -152,8 +152,8 @@ class bookListingTest(BaseCase):
         self.type("#password", "testedPassword1!")
         self.click('#login-button')
         # try to go to the book page by force through html
-        m = self.open(base_url + "/listing/" + "/" + 
-            str(testListing.id) + "/book")
+        s = str(testListing.id)
+        m = self.open(base_url + "/listing/" + "/" + s + "/book")
         self.assert_false(m)
     
     '''
